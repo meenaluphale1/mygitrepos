9513fc09200f54eb7bbb95a3121bf74f916a2f27
MeenalAnand <meenalanand@git.com>
2020-06-30 19:39:11 +0530

    change in code of servelt

--- MyMavenProject/src/main/java/com/meenal/demo/MyServlet.java
+++ MyMavenProject/src/main/java/com/meenal/demo/MyServlet.java
@@ -25,7 +25,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Hello servelts ").append(request.getContextPath());
 	}
 
 	/**
